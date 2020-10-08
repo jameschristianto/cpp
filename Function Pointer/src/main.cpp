79e1c384e82287c37a0a7a53038acc191f7686a4
Mes <jameschrist0000@gmail.com>
2020-10-08 15:06:23 +0700

    C++

--- Function Pointer/src/main.cpp
+++ Function Pointer/src/main.cpp
@@ -22,19 +22,25 @@ int main()
     //Make function pointer
     int (*pinSquare)(int);
     pinSquare = &inSquare;
+    //pinSquare = inSquare; //Another method
 
     void (*pvdPrintMessage)(string);
     pvdPrintMessage = &vdPrintMessage;
+    //pvdPrintMessage = vdPrintMessage; //Another method
 
     int (*pinAdd)(int, int);
     pinAdd = &inAdd;
+    //pinAdd = inAdd; //Another method
 
     //Test function pointer
     cout << "Square of " << i << " is " << pinSquare(i) << endl;
+    //cout << "Square of " << i << " is " << (*pinSquare)(i) << endl; //Another method
 
     pvdPrintMessage("Test Function Pointer");
+    //(*pvdPrintMessage)("Test Function Pointer"); //Another method
 
     cout << "Add of " << j << " and " << k << " is " << pinAdd(j, k) << endl;
+    //cout << "Add of " << j << " and " << k << " is " << (*pinAdd)(j, k) << endl; //Another method
 
     cin.get();
 
