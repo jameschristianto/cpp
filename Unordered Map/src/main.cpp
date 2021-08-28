e8bc1796849450b2a1162c57a11ce4988e76cf24
Mes <jameschrist0000@gmail.com>
2021-08-28 07:46:02 +0700

    C++

---
+++ Unordered Map/src/main.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <utility> //make_pair
+
+using namespace std;
+
+string filePath = "assets/file.txt";
+
+void findValue(unordered_map<int, string> &myMap);
+void print(unordered_map<int, string> &myMap);
+void getSize(unordered_map<int, string> &myMap);
+void insert(unordered_map<int, string> &myMap);
+void erase(unordered_map<int, string> &myMap);
+void clear(unordered_map<int, string> &myMap);
+
+int main()
+{
+    unordered_map<int, string> myMap;
+    pair<int, string> myPair;
+
+    int key;
+    string value;
+
+    string line;
+    ifstream inFile(filePath);
+
+    int select;
+
+    if (inFile.is_open())
+    {
+        cout << "File opened..." << endl;
+        while (getline(inFile, line))
+        {
+            //cout << "ID : " << line.substr(0, line.find(":"));
+            //cout << " , Value : " << line.substr(line.find(":") + 1) << endl;
+
+            key = stoi(line.substr(0, line.find(":")));
+            value = line.substr(line.find(":") + 1);
+
+            //method 1
+            myMap.insert({key, value});
+
+            //method 2
+            //myMap.insert(make_pair(key, value));
+
+            //method 3
+            //myMap[key] = value;
+        }
+        inFile.close();
+    }
+    else
+        cout << "Unable to open file..." << endl;
+
+    do
+    {
+        cout << "1. Print" << endl;
+        cout << "2. Find" << endl;
+        cout << "3. Size" << endl;
+        cout << "4. Insert" << endl;
+        cout << "5. Erase" << endl;
+        cout << "6. Clear" << endl;
+        cout << endl;
+
+        cout << "Select command : ";
+        cin >> select;
+
+        switch (select)
+        {
+        case 1:
+            print(myMap);
+            break;
+        case 2:
+            findValue(myMap);
+            break;
+        case 3:
+            getSize(myMap);
+            break;
+        case 4:
+            insert(myMap);
+            break;
+        case 5:
+            erase(myMap);
+            break;
+        case 6:
+            clear(myMap);
+            break;
+        default:
+            break;
+        }
+    } while (1);
+
+    system("pause");
+    return 0;
+}
+
+void findValue(unordered_map<int, string> &myMap)
+{
+    int selectKey;
+
+    cout << endl;
+    cout << "Input key : ";
+    cin >> selectKey;
+
+    if (myMap.find(selectKey) != myMap.end())
+    {
+        cout << "Value of key " << selectKey << " : " << myMap.find(selectKey)->second << endl;
+        cout << endl;
+        return;
+    }
+
+    cout << "Key not found..." << endl;
+    cout << endl;
+}
+
+void print(unordered_map<int, string> &myMap)
+{
+    unordered_map<int, string>::iterator itr;
+
+    cout << endl;
+
+    if (!myMap.empty())
+    {
+        for (itr = myMap.begin(); itr != myMap.end(); ++itr)
+        {
+            cout << itr->first << " -> " << itr->second << endl;
+        }
+    }
+    else cout << "No data found..." << endl;
+
+    cout << endl;
+}
+
+void getSize(unordered_map<int, string> &myMap)
+{
+    cout << endl;
+    cout << "There is " << myMap.size() << " data" << endl;
+    cout << endl;
+}
+
+void insert(unordered_map<int, string> &myMap)
+{
+    int key;
+    string value;
+
+    cout << endl;
+    cout << "Input key : ";
+    cin >> key;
+    cout << "Input value : ";
+    cin >> value;
+
+    if(myMap.find(key) == myMap.end()) myMap.insert({key, value});
+    else myMap.find(key)->second = value;
+
+    cout << endl;
+}
+void erase(unordered_map<int, string> &myMap)
+{
+    int key;
+    string value;
+    unordered_map<int, string>::iterator itr;
+
+    cout << endl;
+    cout << "Input key : ";
+    cin >> key;
+
+    //method 1
+    myMap.erase(key);
+
+    //method 2
+    //itr = myMap.find(key);
+    //myMap.erase(itr);
+
+    cout << endl;
+}
+
+void clear(unordered_map<int, string> &myMap)
+{
+    myMap.clear();
+    cout << endl;
+}
--- Vector/src/main.cpp
+++ Vector/src/main.cpp
@@ -10,9 +10,9 @@ int main()
 
     //add value to vector (pop_back for delete last value vector)
     intVector.push_back(1);
-    intVector.push_back(2);
+    intVector.push_back(7);
     intVector.push_back(3);
-    intVector.push_back(4);
+    intVector.push_back(2);
     intVector.push_back(5);
 
     //vector size
